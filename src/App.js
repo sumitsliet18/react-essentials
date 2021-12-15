b291b8110b72d7b944b9fd1fd9e8d3bf090eb658
Sumit Kumar <kumarsumit@microsoft.com>
2021-12-15 11:04:56 +0530

    added restaurant image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import restaurant from "./restaurant.jpg";
+function Header(props) {
+  
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+function Main(props) {
+  return (
+    <section>
+      <p>We serve the most {props.adjective} food around.</p>
+      <img src="https://github.com/sumitsliet18" height="200"/>
+      <ul style={{textAlign:"left"}}>
+        {props.dishes.map((dish) => 
+        <li key={dish.id}>{dish.title}</li>
+        )}
+      </ul>
+    </section>
+  );
+}
+function Footer(props) {
+  return (
+    <footer>
+      <p>Copyright {props.year}</p>
+      </footer>
+  );
+}
+
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables",
+  "Paneer"
+];
+
+const dishObject = dishes.map((dish, i) => ({id: i, title: dish}));
+// console.log(dishObject);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name = "Sumit" />
+      <Main adjective = "amazing" dishes={dishObject} />
+      <Footer year = {new Date().getFullYear()} />
+
     </div>
   );
 }
